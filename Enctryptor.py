1b8eca3d8a6eb85ad17a99109a53c6a7f361ec4e
amradhwen <radhweneamri9631@gmail.com>
2023-11-02 15:33:46 +0100

    .

--- Enctryptor.py
+++ Enctryptor.py
@@ -60,3 +60,43 @@ class Encryptor:
             for file in files:
                 file_path = os.path.join(root, file)
                 self.encrypt_file(file_path)
+
+    def decrypt_file(self, encrypted_file):
+        filename, ext = os.path.splitext(os.path.basename(encrypted_file))
+        if ext != '.xv':
+            return  # Skip files that are not encrypted with .xv extension
+
+        original_extension = os.path.splitext(filename)[1]
+        decrypted_file_name = f"{filename.rsplit('.', 1)[0]}{original_extension}"
+        output_dir = os.path.dirname(encrypted_file)
+        decrypted_file_path = os.path.join(output_dir, decrypted_file_name)
+
+        with open(encrypted_file, "rb") as file:
+            encrypted_aes_key = file.read(256)
+            nonce = file.read(16)
+            ciphertext = file.read()
+            aes_key = self.decrypt_aes_key(encrypted_aes_key)
+
+            if aes_key is None:
+                return  # Unable to decrypt the file
+
+            aes_cipher = AES.new(aes_key, AES.MODE_EAX, nonce=nonce)
+            plaintext = aes_cipher.decrypt(ciphertext)
+
+            with open(decrypted_file_path, "wb") as decrypted_file:
+                decrypted_file.write(plaintext)
+
+        os.remove(encrypted_file)
+
+    def decrypt_aes_key(self, encrypted_aes_key):
+        with open(self.private_key_path, "rb") as private_key_file:
+            private_key = RSA.import_key(private_key_file.read())
+            rsa_cipher = PKCS1_OAEP.new(private_key)
+            aes_key = rsa_cipher.decrypt(encrypted_aes_key)
+            return aes_key
+
+    def decrypt_files_in_folder(self, folder_path):
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                self.decrypt_file(file_path)
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     input_folder = "encryptme"
     encryptor = Encryptor()
     encryptor.generate_key_pair()
-    encryptor.encrypt_files_in_folder(input_folder)
+    #encryptor.encrypt_files_in_folder(input_folder)
+    encryptor.decrypt_files_in_folder(input_folder)
 
 
 
